f6e133bdfb1db7f628c8a200c45fc28fb2a88380
Atsuhiro Kou <millerttime60@gmail.com>
2019-08-18 18:02:42 +0800

    # add links to power thesaurus # gathering image files into new folder # add comments relating counts # modify layout of page # add new sentences

--- app.R
+++ app.R
@@ -3,25 +3,51 @@ source("senteces.R")
 
 # Define UI for application that shows English sentences
 ui <- fluidPage(
-   
+  tags$head(tags$style("#value {font-size: 15px;}
+                        #count {font-weight: bold;
+                                font-size: 18px;}
+                        #comment {color: blue; 
+                                  font-size: 15px; 
+                                  font-weight: bold; 
+                                  margin-top: 5px; 
+                                  padding-bottom: 0px}")), 
+  
    # Application title
    titlePanel("Everyday 100 English sentences!!!"),
    
-   # acction button shows count and English sentences randomly
-   actionButton("action", label = "NEXT"), 
+   # action button shows count and English sentences randomly
+   # action button
+   actionButton(
+     "action", label = "NEXT"
+     ), 
    
-   mainPanel(textOutput("count")), 
+   mainPanel(
+     # show the number of English sentences
+     textOutput("count"), 
+     # express a little comments
+     textOutput("comment")
+     ), 
    
    hr(), 
    
-   mainPanel(verbatimTextOutput("value")), 
+   mainPanel(
+     wellPanel(textOutput("value"), 
+               style = "padding: 10px;
+                        margin-bottom: 0px;"), 
+     width = "auto"
+     ), 
    
    br(), 
    
-   mainPanel(a(imageOutput("camb", height = 60), 
+   # links for searching words
+   mainPanel(a(imageOutput("camb", height = 70), 
                href = "https://dictionary.cambridge.org")), 
-   mainPanel(a(imageOutput("goo", height = 60), 
-               href = "https://www.google.co.jp"))
+   
+   mainPanel(a(imageOutput("goo", height = 70), 
+               href = "https://www.google.co.jp")), 
+   
+   mainPanel(a(imageOutput("power", height = 70), 
+               href = "https://www.powerthesaurus.org"))
    )
 
 # Define server logic required to show English sentences
@@ -45,14 +71,40 @@ server <- function(input, output) {
     input$action
   })
   
+  # show comment
+  output$comment <- renderText({
+    case_when(input$action < 25 ~ 
+                "a journey of a thousand miles begins with a single step!", 
+              input$action == 25 ~ 
+                "You reach a quarter of daily task!", 
+              input$action > 25 && input$action < 50  ~ 
+                "Slow and steady wins the race!", 
+              input$action == 50 ~ 
+                "You reach a half of daily task!", 
+              input$action > 50 && input$action < 75 ~ 
+                "Rome was not built in a day!", 
+              input$action == 75 ~ 
+                "You reach three-quarters of daily task!", 
+              input$action > 75 && input$action < 100 ~ 
+                "There's many slips between the cup and the lip!", 
+              input$action >= 100 ~ 
+                "You achieve your daily task! You are awsome!")
+  })
+  
+  # imges of links
   output$camb <- renderImage({
-    list(src = "cambridge.png", 
-         height = 50)
+    list(src = "images/cambridge.png", 
+         width = 150)
   }, deleteFile = FALSE)
   
   output$goo <- renderImage({
-    list(src = "google.png", 
-         height = 50)
+    list(src = "images/google.png", 
+         width = 150)
+  }, deleteFile = FALSE) 
+  
+  output$power <- renderImage({
+    list(src = "images/power.png", 
+         width = 150)
   }, deleteFile = FALSE)
 
 }
